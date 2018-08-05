e26ce1629a8c5265a4ac79dc4cdcffeda39b4041
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 13:58:01 +0300

    python

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,16 @@
 # Write your solution for 1.2 here!
+#a = 0
+
+#for i in range(101):
+#	if i%2==0:
+#		a += i
+    ### 2550 ###
+#print(a)
+
+for i in range(1001):
+	cube_i = i*i*i 
+	if (i%6) == 2 and (cube_i%5) == 3:
+		print(i)
+		### 992 :) ###
+
+
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,7 @@
 # Write your solution for 1.1 here!
+a = 0
+
+for i in range(101):
+	a += i 
+
+print(a)
